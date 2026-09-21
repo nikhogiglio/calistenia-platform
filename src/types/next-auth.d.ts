9dc4fb1999a487b24757d4091199be7926923d07
nikhogiglio <nikholasgiglio15@gmail.com>
2026-09-21 15:13:02 -0400

    Add TypeScript definitions for next-auth session and user

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,25 @@
+import { Role } from "@prisma/client";
+import type { DefaultSession } from "next-auth";
+import "next-auth";
+import "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      role: Role;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
+    role: Role;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    role: Role;
+  }
+}
